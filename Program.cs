79c958106df9e4c4df89e4c4269e3aae13be754c
Mervin Tan (DHL IT Services) <mervin.tan@dhl.com>
2021-08-16 15:59:39 +0800

    First Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("What is your name?");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Nice to meet you {name}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# dotnet-core_helloworld_demo
+Just a simple hello world using .Net Core
